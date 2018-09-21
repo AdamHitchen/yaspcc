1987174d2d700012ab5c9a437b0bad1ddd540fb9
AdamHitchen <adam.hitchen2@gmail.com>
2018-09-21 17:52:33 +0100

    Fixed invalid and missing references in bootstrap file

--- yaspcc/config/bootstrap.php
+++ yaspcc/config/bootstrap.php
@@ -18,9 +18,10 @@ try {
     $container = $containerBuilder->build();
 
     $container->set(\Psr\Log\LoggerInterface::class, $monolog);
-    $container->set(\Yaspcc\Routing\RouterInterface::class, $container->make(\Yaspcc\Routing\SymfonyRouter::class));
+    $container->set(\Yaspcc\Api\Routing\RouterInterface::class, $container->make(\Yaspcc\Api\Routing\SymfonyRouter::class));
     $container->set(\Yaspcc\Cache\Redis\RedisClientInterface::class, $container->make(\Yaspcc\Cache\Redis\Wrapper\PredisWrapper::class));
     $container->set(\Yaspcc\Cache\KeyValueCacheInterface::class, $container->make(\Yaspcc\Cache\Redis\Service\RedisService::class));
+    $container->set(\Yaspcc\Ratings\Service\RatingServiceInterface::class, $container->make(\Yaspcc\Ratings\Service\GoogleSheetsService::class));
 
     return $container;
 
