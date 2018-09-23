79b2ded4c01e8cb6834987f20d4678e377e2555a
AdamHitchen <adam.hitchen2@gmail.com>
2018-09-23 11:30:44 +0100

    misc frontend stuff

--- yaspcc/frontend/src/App.js
+++ yaspcc/frontend/src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Yet Another Steam Play Compatibility Checker</h1>
+                </header>
+                <p className="App-intro">
+                    <div className={"app-container"}>
+                        <div className={"app-image"} style={{float: "left"}}>
+                            <img src={"placeholder.jpg"} data-src={"somewhere-valve-related"}/>
+                        </div>
+                        <div className={"app-title"} style={{"border-bottom": "1px"}}>
+                            asd
+                        </div><br/>
+                        <div>
+                            <p>
+                                Rating
+                            </p>
+
+                        </div>
+                    </div>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
